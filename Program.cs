8ca629b9431461b698ffafbfde8def26a0de6732
Garett Hicks <grhicks@comcast.net>
2020-02-01 15:59:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Author: Garett Hicks
+//Project: Deliverable 1
+//Comments: This C# Console program showcases a WriteLine command to display a "hello world" message.
+
+using System;
+
+namespace Deliverable_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+        }
+    }
+}
